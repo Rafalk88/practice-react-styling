e0ab449443ca77ca3613bc1890e636c0ec6b7463
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-10-15 18:05:33 +0200

    ThemeProvider added

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 import { StyledAlert } from './Alert.styled';
+import { ThemeProvider } from "styled-components";
 
-const Alert = props => {
-    return (
+const theme = {
+  color: "red",
+  backgroundColor: "yellow",
+};
+
+const Alert = (props) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
         <StyledAlert>{props.children}</StyledAlert>
-    );
-}
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Alert;
--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const StyledAlert = styled.div`
-    display: block;
-`
+  display: block;
+  color: ${(props) => props.theme.color};
+  background-color: ${(props) => props.theme.backgroundColor};
+  border: 1px solid ${(props) => props.theme.borderColor};
+`;
 
 export { StyledAlert };
