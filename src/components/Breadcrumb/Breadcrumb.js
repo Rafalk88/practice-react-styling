d4edf8e9660b397970352653e2a1a112936fa80f
Rafalk88 <99990595+Rafalk88@users.noreply.github.com>
2022-10-23 12:29:00 +0200

    breadcrumb.styled implemented

---
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { StyledBreadcrumb } from "./Breadcrumb.styled";
+
+const Breadcrumb = (props) => {
+  return <StyledBreadcrumb>{"Home"}</StyledBreadcrumb>;
+};
+
+export default Object.assign(Breadcrumb, {});
---
+++ src/components/Breadcrumb/Breadcrumb.styled.js
@@ -0,0 +1,11 @@
+import Styled from "styled-components";
+
+export const StyledBreadcrumb = Styled.ol`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0.75rem 1rem;
+  margin-bottom: 1rem;
+  list-style: none;
+  background-color: #e9ecef;
+  border-radius: 0.25rem;
+`;
